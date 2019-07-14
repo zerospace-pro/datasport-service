6e8844af2c38f248653af1e9a3274c9c875ae771
Nathan <Mr.Chenyao@outlook.com>
2019-07-14 20:55:56 +0800

    调整结构

--- service-gateway/src/main/java/com/zerospace/datasport/gateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/com/zerospace/datasport/gateway/ServiceGatewayApplication.java
@@ -1,14 +1,10 @@
 package com.zerospace.datasport.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableAutoConfiguration
 @EnableEurekaClient
-@EnableFeignClients(basePackages = "com.zerospace.datasport.joggle")
 @SpringBootApplication
 public class ServiceGatewayApplication {
 
